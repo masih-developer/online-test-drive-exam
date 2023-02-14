3c51dd836ff48cdca1696f5a8ff4e575c4bb4583
dosepehr <dosepehr@gmail.com>
2023-02-15 01:47:22 +0330

    refactor codes and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React from "react";
-import Header from "./components/common/Header/Header";
-import QuizApp from "./components/layout/QuizApp/QuizApp";
-import Footer from "./components/common/Footer/Footer";
+import React from 'react';
+import { Header, QuizApp, Footer } from './components';
 const App = () => {
     return (
         <>
