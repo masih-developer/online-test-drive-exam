1b8047e5382861d37197cc0bdb81159d2a215420
masih abedini <masih7816@gmail.com>
2023-02-14 22:40:43 +0330

    change image directories

--- src/constants/questionsData.js
+++ src/constants/questionsData.js
@@ -1,407 +1,402 @@
 export const questions = [
     {
         id: 1,
-        img: '/images/q1.jpg',
-        questionTitle: 'هنرجوی گرامی نام خطوط مشخص شده در تصویر بالا چیست؟',
+        img: "./assets/images/q1.jpg",
+        questionTitle: "هنرجوی گرامی نام خطوط مشخص شده در تصویر بالا چیست؟",
         questionOptions: [
-            { answer: 'خطوط زیگزاگ', isCorrect: false },
-            { answer: 'خطوط هاشور', isCorrect: true },
-            { answer: 'خطوط شطرنجی ', isCorrect: false },
-            { answer: 'هیچ کدام', isCorrect: false },
+            { answer: "خطوط زیگزاگ", isCorrect: false },
+            { answer: "خطوط هاشور", isCorrect: true },
+            { answer: "خطوط شطرنجی ", isCorrect: false },
+            { answer: "هیچ کدام", isCorrect: false },
         ],
     },
     {
         id: 2,
-        img: '/images/q2.jpg',
+        img: "./assets/images/q2.jpg",
         questionTitle:
-            'در بزرگراه اتومبیل شما دچار نقص فنی میشود برای هشدار به وسائط نقلیه دیگر تابلوی احتیاط را در چه فاصله ای از اتومبیل خود قرار می دهید؟ ',
+            "در بزرگراه اتومبیل شما دچار نقص فنی میشود برای هشدار به وسائط نقلیه دیگر تابلوی احتیاط را در چه فاصله ای از اتومبیل خود قرار می دهید؟ ",
         questionOptions: [
-            { answer: '100 متری', isCorrect: false },
-            { answer: '200 متری', isCorrect: false },
-            { answer: '150 متری', isCorrect: true },
-            { answer: '70 متری', isCorrect: false },
+            { answer: "100 متری", isCorrect: false },
+            { answer: "200 متری", isCorrect: false },
+            { answer: "150 متری", isCorrect: true },
+            { answer: "70 متری", isCorrect: false },
         ],
     },
     {
         id: 3,
-        img: '/images/q3.jpg',
-        questionTitle: 'در چند متر مانده به پیچ ها سبقت ممنوع است؟',
+        img: "./assets/images/q3.jpg",
+        questionTitle: "در چند متر مانده به پیچ ها سبقت ممنوع است؟",
         questionOptions: [
-            { answer: '100 متری', isCorrect: false },
-            { answer: '50 متری', isCorrect: true },
-            { answer: '150 متری', isCorrect: false },
-            { answer: '60 متری', isCorrect: false },
+            { answer: "100 متری", isCorrect: false },
+            { answer: "50 متری", isCorrect: true },
+            { answer: "150 متری", isCorrect: false },
+            { answer: "60 متری", isCorrect: false },
         ],
     },
     {
         id: 4,
-        img: '/images/q4.jpg',
+        img: "./assets/images/q4.jpg",
         questionTitle:
-            'در تقاطع هم عرض اگر دو وسیله نقلیه ای که روبروی یکدیگر در حرکتند بخواهند وارد خیابان مجاور شوند حق تقدم با کدام است',
+            "در تقاطع هم عرض اگر دو وسیله نقلیه ای که روبروی یکدیگر در حرکتند بخواهند وارد خیابان مجاور شوند حق تقدم با کدام است",
         questionOptions: [
-            { answer: 'وسیله ای که به چپ گردش میکند.', isCorrect: false },
-            { answer: 'وسیله ای که به راست گردش میکند.', isCorrect: true },
+            { answer: "وسیله ای که به چپ گردش میکند.", isCorrect: false },
+            { answer: "وسیله ای که به راست گردش میکند.", isCorrect: true },
             {
-                answer: 'منتظر میمانند تا پلیس راهنمایی بیاید.',
+                answer: "منتظر میمانند تا پلیس راهنمایی بیاید.",
                 isCorrect: false,
             },
-            { answer: ' هیچکدام.', isCorrect: false },
+            { answer: " هیچکدام.", isCorrect: false },
         ],
     },
     {
         id: 5,
-        img: '/images/q5.jpg',
-        questionTitle: 'هنرجوی گرامی نوع پارک تصویر بالا را مشخص کنید',
+        img: "./assets/images/q5.jpg",
+        questionTitle: "هنرجوی گرامی نوع پارک تصویر بالا را مشخص کنید",
         questionOptions: [
-            { answer: 'پارک عمودی', isCorrect: false },
-            { answer: 'پارک مورب', isCorrect: false },
-            { answer: 'خطوط پارک افقی', isCorrect: true },
-            { answer: 'پارک مناسب', isCorrect: false },
+            { answer: "پارک عمودی", isCorrect: false },
+            { answer: "پارک مورب", isCorrect: false },
+            { answer: "خطوط پارک افقی", isCorrect: true },
+            { answer: "پارک مناسب", isCorrect: false },
         ],
     },
     {
         id: 6,
-        img: '/images/q6.jpg',
-        questionTitle: 'در میدان ها حق تقدم عبور با کدام وسیله است؟ ',
+        img: "./assets/images/q6.jpg",
+        questionTitle: "در میدان ها حق تقدم عبور با کدام وسیله است؟ ",
         questionOptions: [
-            { answer: 'وسیله ای اول وارد می شود.', isCorrect: false },
+            { answer: "وسیله ای اول وارد می شود.", isCorrect: false },
             {
-                answer: 'وسیله ای که زودتر وارد میدان شده است.',
+                answer: "وسیله ای که زودتر وارد میدان شده است.",
                 isCorrect: false,
             },
             {
-                answer: 'وسیله ای که در درون میدان در حال حرکت است.',
+                answer: "وسیله ای که در درون میدان در حال حرکت است.",
                 isCorrect: true,
             },
-            { answer: 'هیچکدام', isCorrect: false },
+            { answer: "هیچکدام", isCorrect: false },
         ],
     },
     {
         id: 7,
-        img: '/images/q7.jpg',
-        questionTitle: 'با همان سرعت به راه خود ادامه می دهید.',
+        img: "./assets/images/q7.jpg",
+        questionTitle: "با همان سرعت به راه خود ادامه می دهید.",
         questionOptions: [
             {
-                answer: 'با همان سرعت به راه خود ادامه می دهید.',
+                answer: "با همان سرعت به راه خود ادامه می دهید.",
                 isCorrect: false,
             },
             {
-                answer: 'با احتیاط سرعت را کاهش می دهید.',
+                answer: "با احتیاط سرعت را کاهش می دهید.",
                 isCorrect: true,
             },
             {
-                answer: 'از قسمت چپ جاده عبور می کنید.',
+                answer: "از قسمت چپ جاده عبور می کنید.",
                 isCorrect: false,
             },
             {
-                answer: 'فرصت ندهید عابرین از خیابان عبور کنند.',
+                answer: "فرصت ندهید عابرین از خیابان عبور کنند.",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 8,
-        img: '/images/q8.jpg',
+        img: "./assets/images/q8.jpg",
         questionTitle:
-            'وقتی در خط عبوری که حرکت می کنید اتومبیل ها توقف کرده باشند و راه مسدود شده باشد چه کار باید کرد؟',
+            "وقتی در خط عبوری که حرکت می کنید اتومبیل ها توقف کرده باشند و راه مسدود شده باشد چه کار باید کرد؟",
         questionOptions: [
             {
-                answer: 'شتاب بگیرید که زودتر از آنها عبور کنید.',
+                answer: "شتاب بگیرید که زودتر از آنها عبور کنید.",
                 isCorrect: false,
             },
             {
-                answer: 'به ترافیک جهت مقابل اجازه عبور میدهید.',
+                answer: "به ترافیک جهت مقابل اجازه عبور میدهید.",
                 isCorrect: true,
             },
             {
-                answer: '.به ترافیک مقابل اشاره کنید تا عبور کنند.',
+                answer: ".به ترافیک مقابل اشاره کنید تا عبور کنند.",
                 isCorrect: false,
             },
             {
-                answer: '.به رفتن ادامه میدهید زیرا حق تقدم با شماست.',
+                answer: ".به رفتن ادامه میدهید زیرا حق تقدم با شماست.",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 9,
-        img: '/images/q9.jpg',
+        img: "./assets/images/q9.jpg",
         questionTitle:
-            'چه موقع میتوانید به صورت دنده عقب از یک خیابان فرعی به خیابان اصلی وارد شوید؟',
+            "چه موقع میتوانید به صورت دنده عقب از یک خیابان فرعی به خیابان اصلی وارد شوید؟",
         questionOptions: [
             {
-                answer: 'در همه مواقع',
+                answer: "در همه مواقع",
                 isCorrect: false,
             },
             {
-                answer: 'تنها اگر هردو خیابان خلوت باشد.',
+                answer: "تنها اگر هردو خیابان خلوت باشد.",
                 isCorrect: false,
             },
             {
-                answer: 'تنها اگر خیابان اصلی دارای ترافیک نباشد.',
+                answer: "تنها اگر خیابان اصلی دارای ترافیک نباشد.",
                 isCorrect: false,
             },
             {
-                answer: 'هیچ موقع',
+                answer: "هیچ موقع",
                 isCorrect: true,
             },
         ],
     },
     {
         id: 10,
-        img: '/images/q10.jpg',
-        questionTitle:
-            'در تقاطع بالا حق تقدم را مشخص کنید. ( آمبولانس در حال ماموریت است) ',
+        img: "./assets/images/q10.jpg",
+        questionTitle: "در تقاطع بالا حق تقدم را مشخص کنید. ( آمبولانس در حال ماموریت است) ",
         questionOptions: [
             {
-                answer: 'سبز - آمبولانس - زرد',
+                answer: "سبز - آمبولانس - زرد",
                 isCorrect: false,
             },
             {
-                answer: 'زرد - آمبولانس - سبز',
+                answer: "زرد - آمبولانس - سبز",
                 isCorrect: false,
             },
             {
-                answer: 'آمبولانس- سبز - زرد',
+                answer: "آمبولانس- سبز - زرد",
                 isCorrect: true,
             },
             {
-                answer: 'آمبولانس - زرد - سبز',
+                answer: "آمبولانس - زرد - سبز",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 11,
-        img: '/images/q1.jpg',
-        questionTitle: 'نام نقاط قرمز رنگ مشخص شده در تصویر بالا چیست؟',
+        img: "./assets/images/q1.jpg",
+        questionTitle: "نام نقاط قرمز رنگ مشخص شده در تصویر بالا چیست؟",
         questionOptions: [
             {
-                answer: 'نقاط خطرناک',
+                answer: "نقاط خطرناک",
                 isCorrect: false,
             },
             {
-                answer: 'محدوده نقاط کور در خودروهای سنگین',
+                answer: "محدوده نقاط کور در خودروهای سنگین",
                 isCorrect: false,
             },
             {
-                answer: 'نقطه کور ماشین سنگین',
+                answer: "نقطه کور ماشین سنگین",
                 isCorrect: false,
             },
             {
-                answer: 'همه موارد',
+                answer: "همه موارد",
                 isCorrect: true,
             },
         ],
     },
     {
         id: 12,
-        img: '/images/q12.jpg',
-        questionTitle: 'در تقاطع بالا حق تقدم عبور را مشخص کنید.',
+        img: "./assets/images/q12.jpg",
+        questionTitle: "در تقاطع بالا حق تقدم عبور را مشخص کنید.",
         questionOptions: [
             {
-                answer: 'سواری - کامیون - دوچرخه - موتور سیکلت',
+                answer: "سواری - کامیون - دوچرخه - موتور سیکلت",
                 isCorrect: false,
             },
             {
-                answer: 'موتور سیکلت - دوچرخه - کامیون - سواری',
+                answer: "موتور سیکلت - دوچرخه - کامیون - سواری",
                 isCorrect: false,
             },
             {
-                answer: 'کامیون - دوچرخه - سواری - موتور سیکلت',
+                answer: "کامیون - دوچرخه - سواری - موتور سیکلت",
                 isCorrect: false,
             },
             {
-                answer: 'دوچرخه - موتور سیکلت - کامیون - سواری',
+                answer: "دوچرخه - موتور سیکلت - کامیون - سواری",
                 isCorrect: true,
             },
         ],
     },
     {
         id: 13,
-        img: '/images/q13.jpg',
-        questionTitle: 'در چند متری ورودی تونل ها سبقت گرفتن ممنوع است؟',
+        img: "./assets/images/q13.jpg",
+        questionTitle: "در چند متری ورودی تونل ها سبقت گرفتن ممنوع است؟",
         questionOptions: [
             {
-                answer: '150 متری',
+                answer: "150 متری",
                 isCorrect: false,
             },
             {
-                answer: '200 متری',
+                answer: "200 متری",
                 isCorrect: false,
             },
             {
-                answer: '120 متری',
+                answer: "120 متری",
                 isCorrect: false,
             },
             {
-                answer: '100 متری',
+                answer: "100 متری",
                 isCorrect: true,
             },
         ],
     },
     {
         id: 14,
-        img: '/images/q14.jpg',
-        questionTitle:
-            'حداکثر سرعت در جاده های اصلی برون شهری چند کیلومتر است؟',
+        img: "./assets/images/q14.jpg",
+        questionTitle: "حداکثر سرعت در جاده های اصلی برون شهری چند کیلومتر است؟",
         questionOptions: [
             {
-                answer: 'روزها 95 کیلومتر ، شب 85 کیلومتر',
+                answer: "روزها 95 کیلومتر ، شب 85 کیلومتر",
                 isCorrect: true,
             },
             {
-                answer: 'روزها 110 کیلومتر ، شب 90 کیلومتر',
+                answer: "روزها 110 کیلومتر ، شب 90 کیلومتر",
                 isCorrect: false,
             },
             {
-                answer: 'روزها 90 کیلومتر ، شب 85 کیلومتر',
+                answer: "روزها 90 کیلومتر ، شب 85 کیلومتر",
                 isCorrect: false,
             },
             {
-                answer: 'در روزها 80 کیلومتر ، شب 90 کیلومتر',
+                answer: "در روزها 80 کیلومتر ، شب 90 کیلومتر",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 15,
-        img: '/images/q15.jpg',
+        img: "./assets/images/q15.jpg",
         questionTitle:
-            'آیا رانندگان وسایل نقلیه امدادی در هنگام ماموریت مجاز به عبور از محل ممنوع می باشند؟',
+            "آیا رانندگان وسایل نقلیه امدادی در هنگام ماموریت مجاز به عبور از محل ممنوع می باشند؟",
         questionOptions: [
             {
-                answer: 'به علت خطرات موجود نمی توانند عبور ممنوع بروند.',
+                answer: "به علت خطرات موجود نمی توانند عبور ممنوع بروند.",
                 isCorrect: true,
             },
             {
-                answer: 'بستگی به شرایط دارد.',
+                answer: "بستگی به شرایط دارد.",
                 isCorrect: false,
             },
             {
-                answer: 'در هر شرایطی مجاز هستند.',
+                answer: "در هر شرایطی مجاز هستند.",
                 isCorrect: false,
             },
             {
-                answer: 'در صورتیکه موجب تصادف نشوند.',
+                answer: "در صورتیکه موجب تصادف نشوند.",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 16,
-        img: '/images/q16.jpg',
-        questionTitle:
-            'در چه فاصله ای از اطراف چراغهای راهنمایی و رانندگی توقف ممنوع می باشد؟',
+        img: "./assets/images/q16.jpg",
+        questionTitle: "در چه فاصله ای از اطراف چراغهای راهنمایی و رانندگی توقف ممنوع می باشد؟",
         questionOptions: [
             {
-                answer: '15 متری',
+                answer: "15 متری",
                 isCorrect: true,
             },
             {
-                answer: '25 متری',
+                answer: "25 متری",
                 isCorrect: false,
             },
             {
-                answer: '25 متری',
+                answer: "25 متری",
                 isCorrect: false,
             },
             {
-                answer: '5 متری',
+                answer: "5 متری",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 17,
-        img: '/images/q17.jpg',
-        questionsTitle:
-            'چرا شکسته و کثیف بودن شیشه های خودرو می تواند خطرناک باشد؟',
+        img: "./assets/images/q17.jpg",
+        questionsTitle: "چرا شکسته و کثیف بودن شیشه های خودرو می تواند خطرناک باشد؟",
         questionOptions: [
             {
-                answer: 'چون باعث کاهش انعکاس نور می شود.',
+                answer: "چون باعث کاهش انعکاس نور می شود.",
                 isCorrect: false,
             },
             {
-                answer: 'چون باعث افزایش انعکاس نور می شود.',
+                answer: "چون باعث افزایش انعکاس نور می شود.",
                 isCorrect: false,
             },
             {
-                answer: 'چون باعث کاهش دید می شود.',
+                answer: "چون باعث کاهش دید می شود.",
                 isCorrect: true,
             },
             {
-                answer: 'خطری ندارد.',
+                answer: "خطری ندارد.",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 18,
-        img: '/images/q18.jpg',
-        questionsTitle: 'دور زدن از روی خط پر و یا یک خط ممتد …',
+        img: "./assets/images/q18.jpg",
+        questionsTitle: "دور زدن از روی خط پر و یا یک خط ممتد …",
         questionOptions: [
             {
-                answer: 'ممنوع است',
+                answer: "ممنوع است",
                 isCorrect: true,
             },
             {
-                answer: 'از 150 متری ورودی تونل ها',
+                answer: "از 150 متری ورودی تونل ها",
                 isCorrect: false,
             },
             {
-                answer: 'از 100 متری مانده به پیچ ها',
+                answer: "از 100 متری مانده به پیچ ها",
                 isCorrect: false,
             },
             {
-                answer: 'از 100 متری پس از پیچ ها',
+                answer: "از 100 متری پس از پیچ ها",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 19,
-        img: '/images/q19.jpg',
-        questionsTitle:
-            'در هنگام ایمن کردن صحنه تصادف انجام کدامیک از موارد زیر صحیح نمی باشد؟',
+        img: "./assets/images/q19.jpg",
+        questionsTitle: "در هنگام ایمن کردن صحنه تصادف انجام کدامیک از موارد زیر صحیح نمی باشد؟",
         questionOptions: [
             {
-                answer: 'استفاده از تابلو اعلان خطر در فاصله مناسب از تصادف',
+                answer: "استفاده از تابلو اعلان خطر در فاصله مناسب از تصادف",
                 isCorrect: false,
             },
             {
-                answer: 'پارک کردن خودروی خود در فاصله مناسب از صحنه حادثه',
+                answer: "پارک کردن خودروی خود در فاصله مناسب از صحنه حادثه",
                 isCorrect: false,
             },
             {
-                answer: 'به خطر انداختن خود برای نجات دیگران',
+                answer: "به خطر انداختن خود برای نجات دیگران",
                 isCorrect: true,
             },
             {
-                answer: 'استفاده از وسایل روشنایی برای اعلان خطر در شب',
+                answer: "استفاده از وسایل روشنایی برای اعلان خطر در شب",
                 isCorrect: false,
             },
         ],
     },
     {
         id: 20,
-        img: '/images/q20.jpg',
-        questionsTitle: 'کدامیک از موارد زیر در خصوص رانندگی در شب صحیح است؟',
+        img: "./assets/images/q20.jpg",
+        questionsTitle: "کدامیک از موارد زیر در خصوص رانندگی در شب صحیح است؟",
         questionOptions: [
             {
-                answer: 'با افزایش سرعت در شبها می توان کاهش دید را جبران کرد.',
+                answer: "با افزایش سرعت در شبها می توان کاهش دید را جبران کرد.",
                 isCorrect: false,
             },
             {
-                answer: 'میزان تصادفات در شب کاهش می یابد.',
+                answer: "میزان تصادفات در شب کاهش می یابد.",
                 isCorrect: false,
             },
             {
-                answer: 'در شب زمان کمتری طول می کشد تا راننده موانع موجود را ببیند.',
+                answer: "در شب زمان کمتری طول می کشد تا راننده موانع موجود را ببیند.",
                 isCorrect: false,
             },
             {
-                answer: 'محدوده دید راننده در شب کاهش می یابد.',
+                answer: "محدوده دید راننده در شب کاهش می یابد.",
                 isCorrect: true,
             },
         ],
