c6c4d27056cc03c287041fcf58358f3f52445d1d
masih abedini <masih7816@gmail.com>
2023-02-14 22:29:56 +0330

    add new structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/common/Header/Header";
+import QuizApp from "./components/layout/QuizApp/QuizApp";
+import Footer from "./components/common/Footer/Footer";
 
 const App = () => {
-    return <div>online react app</div>;
+    return (
+        <>
+            <Header />
+            <QuizApp />
+            <Footer />
+        </>
+    );
 };
 
 export default App;
