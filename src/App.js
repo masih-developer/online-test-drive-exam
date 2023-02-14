1246f66baee87f297cb4160964e62e5d4f4cb981
masih abedini <masih7816@gmail.com>
2023-02-14 19:53:00 +0330

    install tailwiind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-    return <div>online react app</div>;
+    return <div className="text-3xl font-bold underline">online react app</div>;
 };
 
 export default App;
