ee5915762b7910cd976303402f3872f17c6de4a8
dosepehr <dosepehr@gmail.com>
2023-02-15 00:16:05 +0330

    done question template style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
-import Header from "./components/common/Header/Header";
-import QuizApp from "./components/layout/QuizApp/QuizApp";
-import Footer from "./components/common/Footer/Footer";
-
+import React from 'react';
+import Header from './components/common/Header/Header';
+import QuizApp from './components/layout/QuizApp/QuizApp';
+import Footer from './components/common/Footer/Footer';
+import Question from './components/QuizApp/Quiz/Question';
 const App = () => {
     return (
         <>
             <Header />
             <QuizApp />
             <Footer />
+            <Question />
         </>
     );
 };
